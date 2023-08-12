a534f24e5b6132a2ef91946131aaa0ae0538c505
Aleksei Smirnov <sibusky@yandex.ru>
2023-08-12 04:27:51 +0300

    feat: add page not found

--- src/pages/page-not-found.jsx
+++ src/pages/page-not-found.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
-import { Container } from '../components/container'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Container } from '../components/container';
 
 export function PageNotFound() {
+  let navigate = useNavigate();
+  const goBack = () => navigate(-1);
+
   return (
-    <Container>page-not-found</Container>
-  )
+    <Container>
+      <h2 className='text title'>404</h2>
+      <p className='text'>Page is not found</p>
+      <button className='button_transparent text blue' onClick={goBack}>
+        Go back
+      </button>
+    </Container>
+  );
 }
